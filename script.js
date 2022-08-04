cd6ef1e76acf9f48ce197eb9437e8c4b206d21c6
nickiwahlers111 <61523696+nickiwahlers111@users.noreply.github.com>
2022-08-04 11:46:09 -0700

    got some annoying functionality figured out.

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ function navHamburger() {
     x.style.display = "block";
   }
 }
+
+
+let form = document.querySelector("#form")
+
+// form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  // Add your code here
+  // event.preventDefault();
+  console.log("linked!")
+  var email = document.getElementById('inputEmail3').value;
+  // var message= document.getElementById('exampleFormControlTextarea1').value;
+  
+  // console.log("======FORM SUBMISSION=====");
+  console.log("EMAIL: "+email);
+  // console.log("MESSAGE: " + message);
+
+
+}
