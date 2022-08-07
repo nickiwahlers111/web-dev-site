e3f7807a1af1a7887be816b62a258e959b52154d
nickiwahlers111 <61523696+nickiwahlers111@users.noreply.github.com>
2022-08-07 00:13:51 -0700

    its getting there. Content is almost done, needs css work

--- script.js
+++ script.js
@@ -15,15 +15,25 @@ let form = document.querySelector("#form")
 
 function handleSubmit(event) {
   // Add your code here
-  // event.preventDefault();
+  event.preventDefault();
   console.log("linked!")
   var email = document.getElementById('inputEmail3').value;
-  // var message= document.getElementById('exampleFormControlTextarea1').value;
+  var message= document.getElementById('exampleFormControlTextarea1').value;
+  var preference = '';
+  if(document.getElementById("gridRadios1").checked){
+    preference = "They liked the site"
+  }
+  else if (document.getElementById("gridRadios2").checked){
+    preference = "They loved the site"
+  }
+  else{
+    preference = "No preference"
+  }
   
   // console.log("======FORM SUBMISSION=====");
   console.log("EMAIL: "+email);
-  // console.log("MESSAGE: " + message);
+  console.log("MESSAGE: " + message);
+  console.log("Preference: "+ preference);
 
 }
 
-fartscroll();
