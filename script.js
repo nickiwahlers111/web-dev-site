4dd2f7a427d517a3f894288c69f317505bd01a67
nickiwahlers111 <61523696+nickiwahlers111@users.noreply.github.com>
2022-12-08 13:03:42 -0800

    commented to test

--- script.js
+++ script.js
@@ -33,33 +33,33 @@ function handleSubmit(event) {
   console.log("Preference: " + preference);
 }
 
-async function injectHTML(filePath, elem) {
-  try {
-    const response = await fetch(filePath);
-    if (!response.ok) {
-      return;
-    }
-    const text = await response.text();
-    elem.innerHTML = text;
-    // reinject all <script> tags
-    // for each <script> tag on injected html
-    elem.querySelectorAll("script").forEach((script) => {
-      // create a new empty <script> tag
-      const newScript = document.createElement("script");
-      // copy an attributes of existing script tag
-      // to the new one
-      Array.from(script.attributes).forEach((attr) =>
-        newScript.setAttribute(attr.name, attr.value)
-      );
-      // inject content of existing script tag
-      // to the new one
-      newScript.appendChild(document.createTextNode(script.innerHTML));
-      // replace existing script tag to the new one
-      script.parentNode.replaceChild(newScript, script);
-    });
-  } catch (err) {
-    console.error(err.message);
-  }
-}
+// async function injectHTML(filePath, elem) {
+//   try {
+//     const response = await fetch(filePath);
+//     if (!response.ok) {
+//       return;
+//     }
+//     const text = await response.text();
+//     elem.innerHTML = text;
+//     // reinject all <script> tags
+//     // for each <script> tag on injected html
+//     elem.querySelectorAll("script").forEach((script) => {
+//       // create a new empty <script> tag
+//       const newScript = document.createElement("script");
+//       // copy an attributes of existing script tag
+//       // to the new one
+//       Array.from(script.attributes).forEach((attr) =>
+//         newScript.setAttribute(attr.name, attr.value)
+//       );
+//       // inject content of existing script tag
+//       // to the new one
+//       newScript.appendChild(document.createTextNode(script.innerHTML));
+//       // replace existing script tag to the new one
+//       script.parentNode.replaceChild(newScript, script);
+//     });
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// }
 
-injectHTML("./header.html", document.querySelector(".header"));
+// injectHTML("./header.html", document.querySelector(".header"));
