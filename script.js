41177f70268ff65442f4e0e307eddb59504bb17e
nickiwahlers111 <61523696+nickiwahlers111@users.noreply.github.com>
2022-12-08 13:05:29 -0800

    cleaned up script.js file: removed unneccesary function, added a few comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+//nav bar functionality display
 function navHamburger() {
   var x = document.getElementById("myLinks");
-  // var x = document.getElementsByClassName("myLinks");
+
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
@@ -10,7 +11,7 @@ function navHamburger() {
 
 let form = document.querySelector("#form");
 
-// form.addEventListener("submit", handleSubmit);
+//function to print form submission to console
 
 function handleSubmit(event) {
   // Add your code here
@@ -27,39 +28,7 @@ function handleSubmit(event) {
     preference = "No preference";
   }
 
-  // console.log("======FORM SUBMISSION=====");
   console.log("EMAIL: " + email);
   console.log("COMPLIMENTS: " + message);
   console.log("Preference: " + preference);
 }
-
-// async function injectHTML(filePath, elem) {
-//   try {
-//     const response = await fetch(filePath);
-//     if (!response.ok) {
-//       return;
-//     }
-//     const text = await response.text();
-//     elem.innerHTML = text;
-//     // reinject all <script> tags
-//     // for each <script> tag on injected html
-//     elem.querySelectorAll("script").forEach((script) => {
-//       // create a new empty <script> tag
-//       const newScript = document.createElement("script");
-//       // copy an attributes of existing script tag
-//       // to the new one
-//       Array.from(script.attributes).forEach((attr) =>
-//         newScript.setAttribute(attr.name, attr.value)
-//       );
-//       // inject content of existing script tag
-//       // to the new one
-//       newScript.appendChild(document.createTextNode(script.innerHTML));
-//       // replace existing script tag to the new one
-//       script.parentNode.replaceChild(newScript, script);
-//     });
-//   } catch (err) {
-//     console.error(err.message);
-//   }
-// }
-
-// injectHTML("./header.html", document.querySelector(".header"));
